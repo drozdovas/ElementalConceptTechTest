7a5d7eea189e05d9d2b6ed5bc9852f32241ed516
drozdovas <dmitrijus.drozdovas@gmail.com>
2024-08-09 14:29:19 +0100

    Created a blank project

---
+++ src/main/java/com/drozdovas/elementalconcepttechtest/ElementalConceptTechTestApplication.java
@@ -0,0 +1,7 @@
+package com.drozdovas.elementalconcepttechtest;
+
+public class ElementalConceptTechTestApplication {
+	public static void main (String[] args){
+		System.out.println("Hello Assessment!");
+	}
+}
