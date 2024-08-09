51ffe110d99aa053613f50c9913592f4dc32db32
drozdovas <dmitrijus.drozdovas@gmail.com>
2024-08-09 15:09:11 +0100

    Updated processing controller to include file consumption and return

--- src/main/java/com/drozdovas/elementalconcepttechtest/controller/FileProcessingController.java
+++ src/main/java/com/drozdovas/elementalconcepttechtest/controller/FileProcessingController.java
@@ -1,16 +1,35 @@
 package com.drozdovas.elementalconcepttechtest.controller;
 
+import com.drozdovas.elementalconcepttechtest.model.OutputRecord;
+import com.drozdovas.elementalconcepttechtest.service.FileProcessingService;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/files")
 public class FileProcessingController {
 
-    @PostMapping("/process")
-    public ResponseEntity<String> processFile(@RequestParam("file") MultipartFile file) {
+    private final FileProcessingService fileProcessingService;
 
-            return ResponseEntity.ok().body("Success");
+    public FileProcessingController(FileProcessingService fileProcessingService) {
+        this.fileProcessingService = fileProcessingService;
+    }
+
+    @PostMapping("/process")
+    public ResponseEntity<List<OutputRecord>> processFile(@RequestParam("file") MultipartFile file) {
+        try {
+            List<OutputRecord> outputRecords = fileProcessingService.processFile(file.getInputStream());
+            return ResponseEntity.ok()
+                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"OutcomeFile.json\"")
+                    .body(outputRecords);
+        } catch (IOException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
     }
 }
