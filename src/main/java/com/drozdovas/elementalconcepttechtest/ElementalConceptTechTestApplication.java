82de5d9004750b415149e8dabe4e1e035c12fe40
drozdovas <dmitrijus.drozdovas@gmail.com>
2024-08-09 14:31:41 +0100

    Setup main app as a spring boot app

--- src/main/java/com/drozdovas/elementalconcepttechtest/ElementalConceptTechTestApplication.java
+++ src/main/java/com/drozdovas/elementalconcepttechtest/ElementalConceptTechTestApplication.java
@@ -1,7 +1,12 @@
 package com.drozdovas.elementalconcepttechtest;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class ElementalConceptTechTestApplication {
-	public static void main (String[] args){
-		System.out.println("Hello Assessment!");
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElementalConceptTechTestApplication.class, args);
 	}
 }
